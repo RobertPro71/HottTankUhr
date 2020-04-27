160cb1e4d47a17cc846fa7d13b02b5cd9b4ceec8
Robert Proeschild <proeschild@gmx.de>
2020-04-27 21:09:05 +0200

    blinking led

--- HottTankUhr/main.cpp
+++ HottTankUhr/main.cpp
@@ -5,17 +5,14 @@
  * Author : Robert
  */ 
 
-#define LED_PIN    (1 << PORTA5)
-#define BUTTON_PIN (1 << PORTA1)
+//#define LED_PIN    (1 << PORTA5)
+//#define BUTTON_PIN (1 << PORTA1)
 
-#define F_CPU    8000000
-#define BAUD     19200
-#define BAUD_TOL 1
-#define USE_2X   0
+#define F_CPU    3333333
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include <util/setbaud.h>
+//#include <util/setbaud.h>
 #include "hottv4.h"
 
 void SetupCpuSpeed ( void );
@@ -27,49 +24,51 @@ uint8_t wuf;
 
 int main(void)
 {
-
-	SetupCpuSpeed();
+ 
+  uint32_t UBR_VAL=((F_CPU+19200 * 8)/(19200 * 16)-1);
+  USART0.BAUD = (uint16_t)(UBR_VAL);
+	
+//	SetupCpuSpeed();
 	hott.setup();
 
 	//Setup dig IO
-	DDRA = (1<<DDA5); //LED Output
+	PORTB.DIRSET = PIN2_bm;
 
-	UDR = 'x';               /* schreibt das Zeichen x auf die Schnittstelle */
+	//UDR = 'x';               /* schreibt das Zeichen x auf die Schnittstelle */
 	
     while (1) 
     {
-		PORTA |= (LED_PIN); //Turns ON All LEDs	
-		_delay_ms(1000); //1 second delay
-		PORTA &= ~(LED_PIN); //Turns OFF All LEDs
-		_delay_ms(1000); //1 second delay
-
-		if(UCSRA & (1<<UDRE))         /* Senden, wenn UDR frei ist                    */
-		{
-			UDR = 'x';               /* schreibt das Zeichen x auf die Schnittstelle */
+		  PORTB.OUTSET = PIN2_bm; //Turns ON All LEDs	
+		  _delay_ms(100); //1 second delay
+		  PORTB.OUTCLR = PIN2_bm; //Turns OFF All LEDs
+		  _delay_ms(100); //1 second delay
+//
+		//if(UCSRA & (1<<UDRE))         /* Senden, wenn UDR frei ist                    */
+		//{
+			//UDR = 'x';               /* schreibt das Zeichen x auf die Schnittstelle */
 		}
 
-	}
 }
 
 void SetupCpuSpeed ( void )
 {
-	CLKPSR = (1<<CLKPS0)|(1<<CLKPS1);
-	CCP    = 0xd8;
-	CLKPSR = 0;   // presc 1
-	CLKPSR = 0;   // presc 1
-	CLKPSR = 0;   // presc 1
-	CLKPSR = 0;   // presc 1
+	//CLKPSR = (1<<CLKPS0)|(1<<CLKPS1);
+	//CCP    = 0xd8;
+	//CLKPSR = 0;   // presc 1
+	//CLKPSR = 0;   // presc 1
+	//CLKPSR = 0;   // presc 1
+	//CLKPSR = 0;   // presc 1
 }
 
 void SetupRS232 ( void )
 {
-	//SETUP RS232
-	UCSRB |= (1<<TXEN);                // UART TX einschalten
-	UBRRH = UBRRH_VALUE;
-	UBRRL = UBRRL_VALUE;
-	#if USE_2X
-	UCSRA |= (1 << U2X);
-	#else
-	UCSRA &= ~(1 << U2X);
-	#endif
+	////SETUP RS232
+	//UCSRB |= (1<<TXEN);                // UART TX einschalten
+	//UBRRH = UBRRH_VALUE;
+	//UBRRL = UBRRL_VALUE;
+	//#if USE_2X
+	//UCSRA |= (1 << U2X);
+	//#else
+	//UCSRA &= ~(1 << U2X);
+	//#endif
 }
